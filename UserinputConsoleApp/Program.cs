d996c7dcaa8a4c2738d8eb86b9b53306ca77eb20
SuchitraChadha <suchitra_i@yahoo.com>
2021-12-08 11:49:08 +0100

    User Inputs console Application

---
+++ UserinputConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace UserinputConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           /* Declaring variables to capture user inputs */
+            string FirstName, LastName, EmailAddr, GoalDesc;
+            /* Capturing First Name */
+            Console.WriteLine("Enter your First Name:");
+            FirstName = Convert.ToString(Console.ReadLine());
+
+            /* Capturing Last Name */
+            Console.WriteLine("Enter your Last Name:");
+            LastName = Convert.ToString(Console.ReadLine());
+
+            /* Capturing Email Address */
+            Console.WriteLine("Enter your Email Address:");
+            EmailAddr = Convert.ToString(Console.ReadLine());
+
+            /* Capturing Goal description */
+            Console.WriteLine("Describe your goal:");
+            GoalDesc = Convert.ToString(Console.ReadLine());
+            
+           /* Console.ReadLine(); */
+            
+
+            /*Console.WriteLine("Hello World!");*/
+
+            
+        }
+    }
+}
